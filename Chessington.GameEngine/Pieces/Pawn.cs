938774effc8334fde42786676ba2cfe2237cd7fc
Beatrice <beatrice.opara@softwire.com>
2020-02-13 14:58:00 +0000

    refactor code

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -17,26 +17,54 @@ namespace Chessington.GameEngine.Pieces
             var col = square.Col;
 
             //if player == black 
+            //if play hasn't move 
+            //Square + 2 
+            //else Square + 1
+            
+            //if player = white
+            //if play hasn't move 
+            //Square + 2 
+            //else Square + 1
+            
             //else player == white
+            List<Square> squares = new List<Square>();
+            
             if (Player == Player.Black)
             {
-                List<Square> squares = new List<Square>
+                squares.Add(Square.At(row + 1, col));
+                if (!HasPlayMoved(row))
                 {
-                    Square.At(row + 1, col),
-                };
-                return squares;
+                    squares.Add(Square.At(row + 2, col) );
+                   
+                }
             }
             else
             {
-                List<Square> squares = new List<Square>
+                squares.Add(Square.At(row - 1, col));
+                if (!HasPlayMoved(row))
                 {
-                    Square.At(row - 1, col),
-                };
-                return squares;
+                    squares.Add(Square.At(row - 2, col));
+                }
             }
-            
+            return squares;
+        }
 
-            
+        private bool HasPlayMoved(int row)
+        {
+            if (Player == Player.Black)
+            {
+                return row != 1;
+            }
+            else
+            {
+                return row != 6;
+            }
+            //if player == black 
+            //if play hasn't move 
+            //Square + 2 (return false)
+            //else Square + 1 (return true)
+            //return square 
+            //origin == 1,0 or origin == 6,0
         }
     }
 }
